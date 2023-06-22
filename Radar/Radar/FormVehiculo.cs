36c1934777ad459dff372433ce5addd7dc58e234
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-06-22 12:22:21 -0300

    solucion

--- Radar/Radar/FormVehiculo.cs
+++ Radar/Radar/FormVehiculo.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Radar
 {
-    public partial class Form1 : Form
+    public partial class FormVehiculo : Form
     {
-        public Form1()
+        public FormVehiculo()
         {
             InitializeComponent();
         }
---
+++ Radar/Radar/Vehiculo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radar
+{
+    class Vehiculo
+    {
+        public string Patente { get; private set; }
+        public double Velocidad { get; private set; }
+        public bool EsOficial { get; private set; }
+
+        public Vehiculo(string patente, double velocidad, bool esOficial) 
+        {
+            Patente = patente;
+            Velocidad = velocidad;
+            EsOficial = esOficial;
+        }
+        public bool VerificarVelocidadInfraccion()
+        {
+            return Velocidad < 50 || Velocidad > 90;
+        }
+    }
+}
